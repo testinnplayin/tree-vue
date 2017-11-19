0ea47651dcb7d5446670c8413244767fdcd446db
R Wood <archaurore@gmail.com>
2017-11-19 10:28:25 +0100

    installed vue-awesome and incorporated Icon into app

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon.vue'
+
 Vue.config.productionTip = false
+Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
